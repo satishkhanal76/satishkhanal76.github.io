01cea6810a2448c2d241aa06e5bc3b11a672a171
satishkhanal76 <satishkhanal76@gmail.com>
2025-07-22 16:53:44 -0400

    Adds sitemap support for SEO.

---
+++ generate-sitemap.js
@@ -0,0 +1,25 @@
+// generate-sitemap.js
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { createWriteStream } from 'fs';
+
+const sitemap = new SitemapStream({ hostname: 'https://satishkhanal.com' });
+
+const routes = [
+  '/',
+  '/POS/',
+  '/chess/',
+  '/Travel-Agency-Website/',
+  '/flappy-bird/',
+  '/tic-tac-toe/',
+];
+
+routes.forEach((url) => {
+  sitemap.write({ url, changefreq: 'monthly', priority: url === '/' ? 1.0 : 0.8 });
+});
+
+sitemap.end();
+
+streamToPromise(sitemap).then((data) => {
+  createWriteStream('./public/sitemap.xml').write(data.toString());
+  console.log('✅ Sitemap generated at public/sitemap.xml');
+});
